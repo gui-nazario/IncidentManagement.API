7099e58060c2961832423b9c985e7dc418b22272
saaudavelist <saaudavelist@gmail.com>
2026-02-27 10:20:30 -0300

    Initial commit - API with JWT authentication and role authorization

--- IncidentManagement.API/Program.cs
+++ IncidentManagement.API/Program.cs
@@ -1,26 +1,40 @@
 using IncidentManagement.API.Application.Services;
 using IncidentManagement.API.Infrastructure.Data;
 using IncidentManagement.API.Infrastructure.Repositories;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// =================
+// SWAGGER SIMPLES
+// =================
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
+// =================
+// DATABASE
+// =================
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
+// =================
+// DEPENDENCY INJECTION
+// =================
 builder.Services.AddScoped<IStoreRepository, StoreRepository>();
 builder.Services.AddScoped<IStoreService, StoreService>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
 builder.Services.AddScoped<IStoreFinancialRepository, StoreFinancialRepository>();
 
+// =================
+// JWT
+// =================
 var jwtSettings = builder.Configuration.GetSection("Jwt");
 var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]!);
 
@@ -33,6 +47,7 @@ builder.Services.AddAuthentication(options =>
 {
     options.RequireHttpsMetadata = false;
     options.SaveToken = true;
+
     options.TokenValidationParameters = new TokenValidationParameters
     {
         ValidateIssuer = true,
@@ -47,13 +62,17 @@ builder.Services.AddAuthentication(options =>
 
 var app = builder.Build();
 
+// =================
+// MIDDLEWARE
+// =================
 app.UseMiddleware<IncidentManagement.API.Middleware.ExceptionMiddleware>();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
